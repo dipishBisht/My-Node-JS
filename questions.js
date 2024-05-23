247bcb00f9bdf139bb1c30a21856556298a33c44
dipishBisht <dipish1718@gmail.com>
2024-05-23 13:33:55 +0530

    Practise questions in node

---
+++ questions.js
@@ -0,0 +1,53 @@
+// *    01-js -> easy -> Anagram Question
+
+// const anagram = require("./assignments/01-js/easy/anagram");
+// console.log(anagram("dacb", "cbda"));
+
+// *    01-js -> easy -> Expenditure Question
+
+// const expenditure=require("./assignments/01-js/easy/expenditure-analysis");
+// const temp=
+// [
+//     {
+//     id: 1,
+//     timestamp: 1656076800000,
+//     price: 150,
+//     category: 'Food',
+//     itemName: 'Pizza',
+//   },
+//   {
+//     id: 2,
+//     timestamp: 1756076800000,
+//     price: 50,
+//     category: 'Food',
+//     itemName: 'Burger',
+//   },
+//   {
+//     id: 3,
+//     timestamp: 1856076800000,
+//     price: 199,
+//     category: 'Food',
+//     itemName: 'Pasta',
+//   },{
+//     id: 4,
+//     timestamp: 1956076800000,
+//     price: 120,
+//     category: 'Food',
+//     itemName: 'Biryani',
+//   }
+//   ,{
+//     id: 5,
+//     timestamp: 2056076800000,
+//     price: 60,
+//     category: 'Food',
+//     itemName: 'Sandwich',
+//   }
+// ];
+// console.log(expenditure(temp));
+
+// *    01-js -> easy -> Find Largest Number Question
+
+// const largest = require("./assignments/01-js/easy/findLargestElement");
+// const temp = [1, 5, 2, 8, 3, 6, 4];
+
+// console.log(largest(temp));
