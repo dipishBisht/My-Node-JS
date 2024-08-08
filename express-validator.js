a5e9ae04c1e17d753a99baa3ee8a09f8eeec8a49
dipishBisht <dipish1718@gmail.com>
2024-08-08 19:01:34 +0530

    express validator added

---
+++ express-validator.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const { query, validationResult, matchedData } = require("express-validator")
+const app = express();
+
+app.get("/", query('name').notEmpty().escape(), (req, res) => {
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+        const data = matchedData(req);
+        console.log(data);
+        return res.send(`Hello, ${data.name}!`);
+    }
+
+    res.send({ errors: result.array() });
+})
+
+app.listen("3000", () => console.log("Server started at port 3000"))
