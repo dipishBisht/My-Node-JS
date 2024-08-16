cdd0c30ac2c9bb55e30ce039b11135f9fc20169d
dipishBisht <dipish1718@gmail.com>
2024-08-16 16:27:01 +0530

    connected the server to database

--- MongoDB/restAPI Mongo.js
+++ MongoDB/restAPI Mongo.js
@@ -10,6 +10,18 @@ mongoose.connect("mongodb://127.0.0.1:27017/users-data")
     .then(() => console.log("MongoDB Connected"))
     .catch((err) => console.log("Error:", err));
 
+
+
+// * Event Listners
+
+//#  const db = mongoose.connection;
+
+//#  db.on("connected", () => console.log("Connected to MongoDB server."))
+
+//#  db.on("error", (err) => console.error("MongoDB connection error :", err))
+
+//#  db.on("disconnected", () => console.log("Disconnected to MongoDB server."))
+
 //* Schema
 
 const userSchema = new mongoose.Schema({
